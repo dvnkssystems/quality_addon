7656edc21d0e235f7f302c7698a82e6ef66083b4
aniketdvnks <aniket@dvnks.com>
2026-09-09 12:39:22 +0530

    QCP

---
+++ quality_addon/quality_addon/doctype/inspection_activity/inspection_activity.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, DVNKS Systems Pvt Lts and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Inspection Activity", {
+// 	refresh(frm) {
+
+// 	},
+// });
